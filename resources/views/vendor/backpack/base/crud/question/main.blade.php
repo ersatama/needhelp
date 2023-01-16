c30f00e0d163c106cbb1257df168b8c99e80bdf1
ersatama <ersatama@gmail.com>
2023-01-16 22:59:06 +0600

    wooppay operation_id

--- resources/views/vendor/backpack/base/crud/question/main.blade.php
+++ resources/views/vendor/backpack/base/crud/question/main.blade.php
@@ -232,7 +232,7 @@
                                     <div class="modal-body-item-key text-muted">Цена</div>
                                     <div class="modal-body-item-value">@{{ answered_view.price }}</div>
                                 </div>
-                                <div class="modal-body-item" v-if="answered_view.wooppay">
+                                <div class="modal-body-item border-top" v-if="answered_view.wooppay">
                                     <div class="modal-body-item-key text-muted">ID платежа (wooppay)</div>
                                     <div class="modal-body-item-value">@{{ answered_view.wooppay.operation_id }}</div>
                                 </div>
