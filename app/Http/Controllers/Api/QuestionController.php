5d7d481a156b297cb789193dd179b6c94f43f3e9
ersatama <ersatama@gmail.com>
2023-01-19 10:02:24 +0600

    fixed update repository

--- app/Http/Controllers/Api/QuestionController.php
+++ app/Http/Controllers/Api/QuestionController.php
@@ -187,23 +187,21 @@ class QuestionController extends Controller
         if ($question = $this->questionService->questionRepository->firstById($id)) {
             if ($question->{Contract::ANSWER} && $question->{Contract::ANSWERED_AT}) {
                 return response(ErrorContract::QUESTION_ALREADY_ANSWERED, 400);
-            } else {
-                if ($question->{Contract::STATUS} !== 2) {
-                    if (array_key_exists(Contract::ANSWER, $data)) {
-                        $data[Contract::STATUS] =   2;
-                    }
-                    $question   =   $this->questionService->questionRepository->update($id, $data);
-                    if (array_key_exists(Contract::ANSWER, $data)) {
-                        $this->notificationService->notificationRepository->create([
-                            Contract::USER_ID   =>  $question->{Contract::USER_ID},
-                            Contract::TYPE  =>  1,
-                            Contract::QUESTION_ID   =>  $question->{Contract::ID},
-                            Contract::STATUS    =>  true
-                        ]);
-                    }
+            } elseif ($question->{Contract::STATUS} !== 2) {
+                if (array_key_exists(Contract::ANSWER, $data)) {
+                    $data[Contract::STATUS] =   2;
                 }
+                $question   =   $this->questionService->questionRepository->update($id, $data);
+                if (array_key_exists(Contract::ANSWER, $data)) {
+                    $this->notificationService->notificationRepository->create([
+                        Contract::USER_ID   =>  $question->{Contract::USER_ID},
+                        Contract::TYPE  =>  1,
+                        Contract::QUESTION_ID   =>  $question->{Contract::ID},
+                        Contract::STATUS    =>  true
+                    ]);
+                }
+                event(new QuestionEvent($question));
             }
-            event(new QuestionEvent($question));
             return new QuestionResource($question);
         }
         return response(ErrorContract::NOT_FOUND, 404);
