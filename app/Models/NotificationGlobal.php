402d064962906bbc4912fad37d7eb2a6eeaf75e7
ersatama <ersatama@gmail.com>
2023-01-23 23:25:43 +0600

    global notification beta

--- app/Models/NotificationGlobal.php
+++ app/Models/NotificationGlobal.php
@@ -3,13 +3,13 @@
 namespace App\Models;
 
 use App\Domain\Contracts\NotificationGlobalContract;
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class NotificationGlobal extends Model
 {
-    use \Backpack\CRUD\app\Models\Traits\CrudTrait;
-    use HasFactory;
+    use HasFactory, CrudTrait;
 
     protected $table    =   NotificationGlobalContract::TABLE;
     protected $fillable =   NotificationGlobalContract::FILLABLE;
